67c2a3aa7284e40d943c9d7636f41e7485de49d1
difusedosmosis-ship-it <difusedosmosis@gmail.com>
2026-02-27 20:51:55 +0100

    Create reviews.validators.ts

---
+++ src/modules/reviews/reviews.validators.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const CreateReviewSchema = z.object({
+  requestId: z.string().min(3),
+  vendorUserId: z.string().min(3),
+  rating: z.number().int().min(1).max(5),
+  comment: z.string().optional()
+});
