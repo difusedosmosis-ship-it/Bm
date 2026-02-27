9ef6106fc370869328c5e664dc942cf3a38e794b
difusedosmosis-ship-it <difusedosmosis@gmail.com>
2026-02-27 20:34:04 +0100

    Create server.ts

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { errorMiddleware } from "./middleware/error.js";
+
+import { authRoutes } from "./modules/auth/auth.routes.js";
+import { usersRoutes } from "./modules/users/users.routes.js";
+import { adminRoutes } from "./modules/admin/admin.routes.js";
+import { categoriesRoutes } from "./modules/categories/categories.routes.js";
+import { vendorRoutes } from "./modules/vendor/vendor.routes.js";
+import { requestsRoutes } from "./modules/requests/requests.routes.js";
+import { reviewsRoutes } from "./modules/reviews/reviews.routes.js";
+import { walletRoutes } from "./modules/wallet/wallet.routes.js";
+
+export function buildServer() {
+  const app = express();
+  app.use(helmet());
+  app.use(cors({ origin: true, credentials: true }));
+  app.use(express.json({ limit: "2mb" }));
+  app.use(morgan("dev"));
+
+  app.get("/health", (_req, res) => res.json({ ok: true }));
+
+  app.use("/auth", authRoutes());
+  app.use("/users", usersRoutes());
+  app.use("/admin", adminRoutes());
+  app.use("/categories", categoriesRoutes());
+  app.use("/vendor", vendorRoutes());
+  app.use("/requests", requestsRoutes());
+  app.use("/reviews", reviewsRoutes());
+  app.use("/wallet", walletRoutes());
+
+  app.use(errorMiddleware);
+  return app;
+}
